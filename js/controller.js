cd3d9f7a3d2cdde559944ba1a152e0df258245a4
Robert Bunch <rdbunch@gmail.com>
2015-12-22 10:07:05 -0500

    Add some error and success handling

--- js/controller.js
+++ js/controller.js
@@ -3,11 +3,11 @@ var ecommerceApp = angular.module('ecommerce', []);
 ecommerceApp.controller('ecommerce-controller', function ($scope, $http){
 	$scope.ninja = "DigitalCrafts students";
 
-	$scope.weatherJSON = [];
-	$http.get('http://localhost:3000').then(function (theData){
-		$scope.weatherJSON = theData.data;
-		// console.log(theData.data)
-	})
+	// $scope.weatherJSON = [];
+	// $http.get('http://localhost:3000').then(function (theData){
+	// 	$scope.weatherJSON = theData.data;
+	// 	// console.log(theData.data)
+	// })
 
 
 	//This will fire on ng-submit. We want to make a post request to the server
@@ -18,13 +18,16 @@ ecommerceApp.controller('ecommerce-controller', function ($scope, $http){
 	  $http.post('http://localhost:3000/register', {username: $scope.username, password: $scope.password})
 	    // handle success
 	    .success(function (data, status) {
-			if(data.status = "Registration Successful"){
+	    	if(data.err){
+	    		$scope.loggedin = false; //Keep the form visible so they can try again
+	    		$scope.message = data.err;
+	    	}
+			if(data.status){
 				//do angualar stuff
 				console.log("success");
-				$scope.loggedin = true;
-			} else {
-	        	user = false;
-	      }
+				$scope.loggedin = true; //Hide the login form because they made it through autehnetication
+				$scope.success = data.status
+			}
 	    })
 	    // handle error
 	    .error(function (data) {
